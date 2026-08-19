b1f3ea650b910ff181bd2eb29ae5962522143e46
mikolinol <aminaadilkhan04@gmail.com>
2026-08-19 17:37:31 +0500

    group repo queries

--- src/main/java/com/example/demo/repository/domain/GroupRepository.java
+++ src/main/java/com/example/demo/repository/domain/GroupRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository.domain;
 
 import com.example.demo.model.Group;
+import com.example.demo.model.GroupStudents;
 import com.example.demo.model.Student;
 
 import java.sql.Connection;
 
 public interface GroupRepository {
     Group add(Connection connection, Group g) throws Exception;
-    boolean archive(Connection connection, Long groupId) throws Exception;
+//    boolean archive(Connection connection, Long groupId) throws Exception;
+    GroupStudents getGroupById(Long id) throws Exception;
 
 }
--- src/main/java/com/example/demo/repository/persistence/GroupJRepository.java
+++ src/main/java/com/example/demo/repository/persistence/GroupJRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository.persistence;
 
 import com.example.demo.model.Group;
+import com.example.demo.model.GroupStudents;
 import com.example.demo.repository.domain.GroupRepository;
 
 import java.sql.Connection;
@@ -9,11 +10,24 @@ public class GroupJRepository implements GroupRepository {
 
     @Override
     public Group add(Connection connection, Group g) throws Exception {
+        String sql = "INSERT INTO groups (name, year) VALUES (?,?)";
+
         return null;
     }
 
+//    @Override
+//    public boolean archive(Connection connection, Long groupId) throws Exception {
+//        return false;
+//    }
+
     @Override
-    public boolean archive(Connection connection, Long groupId) throws Exception {
-        return false;
+    public GroupStudents getGroupById(Long id) throws Exception {
+        String sql = """
+                SELECT g.group_id, s.student_id from students s
+                WHERE s.group_id=?
+                JOIN groups g ON s.group_id=g.group_id                
+                """;
+
+        return null;
     }
 }
