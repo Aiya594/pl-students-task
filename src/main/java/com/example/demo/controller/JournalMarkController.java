40683623dba4f78c6f4415ca802000d1e7d1577b
mikolinol <aminaadilkhan04@gmail.com>
2026-08-28 08:43:45 +0500

    journal mark controller

--- src/main/java/com/example/demo/controller/JournalMarkController.java
+++ src/main/java/com/example/demo/controller/JournalMarkController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.model.JournalMark;
+import com.example.demo.service.JournalMarkService;
 import com.example.demo.service.JournalService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(value = "*")
@@ -13,6 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class JournalMarkController {
 
-    private final JournalService journalMarkService;
+    private final JournalMarkService journalMarkService;
 
+    @PostMapping
+    public JournalMark add(@RequestBody JournalMark journalMark) {
+        return journalMarkService.add(journalMark);
+    }
+
+    @PutMapping("/{id}")
+    public JournalMark updateMark(
+            @PathVariable Long id,
+            @RequestParam int mark
+    ) {
+        return journalMarkService.updateMark(id, mark);
+    }
+
+    @GetMapping("/journal/{journalId}")
+    public List<JournalMark> getMarks(
+            @PathVariable Long journalId
+    ) {
+        return journalMarkService.getMarks(journalId);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Long id) {
+        return journalMarkService.delete(id);
+    }
 }
